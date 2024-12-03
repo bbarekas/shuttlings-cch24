43b05a5df391e7fe5922ae5092f296e697ce7111
Bill Barekas <bbarekas@gmail.com>
2024-12-03 16:29:19 +0200

    Add Day -1 solution.

---
+++ README.md
@@ -0,0 +1,10 @@
+## Shuttle's Christmas Code Hunt 2024
+
+Let's start! 
+
+[Shuttle's Christmas Code Hunt 2024
+](https://console.shuttle.dev/shuttlings/cch24)
+
+### Status
+
+* Day -1: Done
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::get,
+    http::{StatusCode},
+    response::{IntoResponse},
+    Router
+};
 
 async fn hello_world() -> &'static str {
-    "Hello, world!"
+    "Hello, bird!"
 }
+
+async fn handle_redirect() -> impl IntoResponse {
+    return (
+        StatusCode::FOUND,
+        [("location", "https://www.youtube.com/watch?v=9Gc4QTqslN4")],
+    );
+    // This returns 303 ...
+    // Redirect::to("https://www.youtube.com/watch?v=9Gc4QTqslN4").into_response()
+}
+
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/seek", get(handle_redirect));
 
     Ok(router.into())
 }
